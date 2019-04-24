db11eaf663747ce210b6bcfef59c9d64d7280b2b
Romeu Antunes <e795750@mail.caixa>
2019-04-24 14:35:10 -0300

    Exercicios Codeforces

--- contest/17-04-2019/F.cpp
+++ contest/17-04-2019/F.cpp
@@ -5,5 +5,32 @@ using namespace std;
 int main(){
 
 
+  std::vector<pair<int,int>> v;
+  int quantidade;
+  int necessidadeBalas;
+  int menosBalas;
+
+  cin >> quantidade;
+  cin >> necessidadeBalas;
+
+  for(int i=0; i<quantidade; i++){
+      cin >> necessidadeBalas;
+      v.push_back(make_pair(necessidadeBalas, i));
+  }
+
+  int j=0;
+
+  do {
+
+    sort(v.begin(),v.end());
+    if(v[j].first<=0){
+
+    }
+    j++;
+  } while(v.size()!=1);
+
+  std::cout << v[0].first << '\n';
+  std::cout << v[0].second+1 << '\n';
+
   return 0;
 }
--- contest/17-04-2019/K.cpp
+++ contest/17-04-2019/K.cpp
@@ -8,22 +8,41 @@ int main(){
   int quantidade;
   int aux;
   int idx1, idx2;
+  int resultado;
   int menor = 1000000000;
 
+
+  idx1 = 0;
+  idx2 = 0;
   cin >> quantidade;
   for(int i=0; i<quantidade; i++){
     cin >> aux;
     v.push_back(make_pair(aux,i));
-
   }
 
   sort(v.begin(),v.end());
 
-  // VERIDICAR AS DIFERENCAS DAS POSICOE PARA ACHAR A MENOR
+  for(int i=0; i<quantidade; i++){
+    if(v[i].second == quantidade-1){
+      resultado = v[i].first - v[0].first;
+      if(resultado <=menor){
+        menor = resultado;
+        idx1 = v[i].second;
+        idx2 = v[0]. second;
+        std::cout << idx1+1 << " " << idx2+1 << '\n';
+        return 0;
+      }
+    } else {
+      resultado = v[i+1].first - v[i].first;
+      if (resultado < menor) {
+        menor = resultado;
+        idx1 = v[i+1].second;
+        idx2 = v[i].second;
+      }
+    }
+  }
 
-
-
-
+  std::cout << idx1+1 << " " << idx2+1<< '\n';
 
   return 0;
 }
