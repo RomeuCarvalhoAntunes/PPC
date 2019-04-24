9ca50ab4f193375da0b449d0b2332157e3333b68
Romeu Antunes <e795750@mail.caixa>
2019-04-24 11:20:21 -0300

    exercicios Codeforces

--- codeforces/122A-LuckyDivision.cpp
+++ codeforces/122A-LuckyDivision.cpp
@@ -4,22 +4,28 @@ using namespace std;
 
 int main(){
 
-  string numero;
+  int numero;
+  vector<int> v{4,7,47,74,444,447,474,477,744,747,774,777};
+  bool lucky = false;
+
   cin >> numero;
 
-  bool lucky = true;
+  auto it =  find(v.begin(),v.end(),numero);
 
-
-  for(int i=0; i < numero.length(); i++){
-    if(numero[i] != '4' && numero[i] != '7'){
-      lucky = false;
-    }
-  }
-
-  if(lucky){
-    std::cout << "YES" << '\n';
+  if(it != v.end()){
+    lucky = true;
+    cout << "YES" << '\n';
   } else {
-    std::cout << "NO" << '\n';
+    for(int i=0; i<v.size(); i++){
+      if(numero%v.at(i) == 0){
+        lucky = true;
+        cout << "YES" << '\n';
+        return 0;
+      } else {
+        lucky = false;
+      }
+    }
+    cout << "NO" << '\n';
   }
 
 
